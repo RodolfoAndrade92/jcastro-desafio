4154a36ae6390ba955c452dc53f66d7abcc4bb98
rodolfo.andrade <rodolfoandrade_@live.com>
2025-02-12 22:29:12 -0300

    Initial commit

---
+++ src/main/java/com/test/dev/contact/manager/ContactManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.test.dev.contact.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/dev/contact/manager/ContactManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.dev.contact.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
